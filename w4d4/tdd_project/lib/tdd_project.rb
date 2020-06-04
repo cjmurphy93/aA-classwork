3296d19ddff24fb184c24dcece72047ec07109c9
cjmurphy93 <cjmurph93@gmail.com>
2020-06-04 15:40:44 -0400

    Starting poker

--- w4d4/tdd_project/lib/tdd_project.rb
+++ w4d4/tdd_project/lib/tdd_project.rb
@@ -1,3 +1,51 @@
+class Array
+
 def my_uniq
-    
+    new_arr = []
+    self.each do |ele|
+        new_arr << ele if !new_arr.include?(ele)
+    end
+    new_arr
+end
+
+
+def two_sum
+    new_arr = []
+    self.each_with_index do |l1, i|
+        self.each_with_index do |l2, j|
+            new_arr << [i,j] if l1 + l2 == 0 && i < j 
+        end
+    end
+    new_arr
+end
+
+def my_transpose
+    transposed = []
+    (0...length).each do |i|
+        new_arr = []
+        (0...length).each do |j|
+            new_arr << self[j][i]
+        end
+        transposed << new_arr
+    end
+    transposed
+end
+
+
+
+def stock_picker
+    indices = []
+    maxprofit = 0
+    self.each_with_index do |amt1, day1|
+        self.each_with_index do |amt2, day2|
+            profit = amt2 - amt1 
+            if profit > maxprofit && day2 > day1
+                maxprofit = profit 
+                indices = [day1, day2]
+            end
+        end
+    end
+    indices
+end
+
 end
--- w4d4/tdd_project/spec/tdd_project_spec.rb
+++ w4d4/tdd_project/spec/tdd_project_spec.rb
@@ -11,3 +11,76 @@ describe "#my_uniq" do
     end
     
 end
+
+describe "#two_sum" do
+    it "adds" do
+    new_arr = [1,1,2,3, -1, 3]
+    expect(new_arr.two_sum).to eq([[0, 4], [1, 4]])
+    end
+
+    it "puts them in order" do
+        new_arr = [1, -2,4,2,3, -1, -3]
+        expect(new_arr.two_sum).to eq([[0, 5], [1,3], [4,6]])
+
+    end
+
+end
+
+describe "#my_transpose" do
+    it "transposes the array" do 
+        rows = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+                ]
+
+        cols =  [ [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+                    ]
+    expect(rows.my_transpose).to eq(cols)
+    end
+    it "returns a new array" do
+        rows = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+                ]
+        expect(rows.my_transpose).not_to be(rows)
+    end
+
+end
+describe "#stock_picker" do
+    it "returns the most profitable pair of days" do 
+        expect([100, 50, 60, 60, 80].stock_picker).to eq([1,4])
+    end
+
+    it "should be in order" do
+        a = [100, 50, 60, 60, 80].shuffle!.stock_picker
+        expect(a[0]).to be < a[1]
+    end
+end
+
+# describe Towers do
+#     subject(:game) {Towers.new }
+#     describe "#initialize" do
+#         it "should create a board of size 3" do
+#             expect(game.board.length).to eq(3)
+#         end
+
+#         it "should set the first array in order largest to smallest" do
+#             expect(game.board[0]).to eq(game.board[0].sort.reverse)
+#         end
+  
+#     end
+
+#     describe "#move" do
+#         it "takes of the end piece" do
+#             expect(game.board[0]).to receive(:pop).with(game)
+#             game.move
+#         end
+
+
+#     end
+# end
+
