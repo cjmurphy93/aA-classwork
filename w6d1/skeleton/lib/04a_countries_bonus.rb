916ce64f3a47f843f15aa3b0a2b7c4c1ac081f01
cjmurphy93 <cjmurph93@gmail.com>
2020-06-15 17:17:16 -0400

    moving on before finsihing bonus problems

--- w6d1/skeleton/lib/04a_countries_bonus.rb
+++ w6d1/skeleton/lib/04a_countries_bonus.rb
@@ -13,7 +13,36 @@ require_relative './sqlzoo.rb'
 def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
+  # execute(<<-SQL)
+  # # SELECT
+  # #   c1.name
+  # # FROM
+  # #   countries AS c1
+  # # WHERE
+  # # c1.gdp > (
+  # #   SELECT
+  # #   MAX(c2.gdp)
+  # #   FROM
+  # #   countries c2
+  # #   WHERE
+  # #   continent = 'Europe'
+  # # )
+  # SQL
+
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      countries.gdp > (
+        SELECT
+          MAX(countries.gdp)
+        FROM
+          countries
+        WHERE
+          countries.continent = 'Europe'
+      )
   SQL
 end
 
@@ -21,6 +50,22 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT
+  c1.continent,
+  c1.name,
+  c1.area
+  FROM
+  countries c1
+  WHERE
+    c1.area = (
+      SELECT
+      MAX(c2.area)
+      FROM
+      countries c2
+      WHERE
+      c1.continent = c2.continent
+    )
+
   SQL
 end
 
@@ -28,5 +73,21 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT
+  c1.name,
+  c1.continent
+  FROM
+  countries c1
+  WHERE
+  c1.population > 3 * (
+    SELECT
+    MAX(c2.population)
+    FROM
+    countries c2
+    WHERE
+    c1.name != c2.name
+    AND
+    c1.continent = c2.continent
+  )
   SQL
 end
